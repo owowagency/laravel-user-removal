5964af6c623b766a040143eb1c0cbe2f6837e14d
YouneselBarnoussi <younes_barnoussi@live.nl>
2020-09-14 16:27:54 +0200

    Laravel 8 support

--- tests/PackageTestCase.php
+++ tests/PackageTestCase.php
@@ -15,7 +15,7 @@ abstract class PackageTestCase extends TestCase
      *
      * @return void
      */
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
